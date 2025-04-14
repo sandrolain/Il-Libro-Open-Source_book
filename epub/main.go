3c8c50e08360faed99323f043b4d7ea9c3e18133
Sandro Lain <sandrolain@outlook.com>
2025-04-14 21:02:04 +0200

    feat: implementa la generazione epub

---
+++ epub/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bytes"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+	"slices"
+	"strings"
+
+	chromahtml "github.com/alecthomas/chroma/v2/formatters/html"
+	epub "github.com/go-shiori/go-epub"
+	img64 "github.com/tenkoh/goldmark-img64"
+	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer/html"
+	"go.abhg.dev/goldmark/frontmatter"
+)
+
+func main() {
+	// Create a new EPUB book
+	book, err := epub.NewEpub("Il Libro Open Source")
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the title and author
+	book.SetTitle("Il manuale del buon dev")
+	book.SetAuthor("Community")
+
+	coverPath, err := book.AddImage("./cover.jpg", "cover.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	cssPath, err := book.AddCSS("./style.css", "style.css")
+	if err != nil {
+		panic(err)
+	}
+
+	book.SetCover(coverPath, "")
+
+	//uid := "https://il-libro-open-source.github.io/book/"
+	//book.SetIdentifier(uid)
+	uid := "f9298b0f-bea1-4cb6-a601-2a35027bd44e"
+	book.SetIdentifier("urn:uuid:" + uid)
+	book.SetLang("it")
+
+	mdPath := "../docs/it"
+
+	actPath, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	cv := goldmark.New(
+		goldmark.WithExtensions(
+			extension.Table,
+			&frontmatter.Extender{},
+			img64.Img64,
+			highlighting.NewHighlighting(
+				highlighting.WithStyle("monokai"),
+				highlighting.WithFormatOptions(
+					chromahtml.WithLineNumbers(true),
+					chromahtml.WrapLongLines(true),
+					chromahtml.TabWidth(2),
+				),
+			),
+		),
+		goldmark.WithRendererOptions(
+			html.WithXHTML(),
+			html.WithUnsafe(),
+			img64.WithPathResolver(func(s string) string {
+				return strings.Replace(s, "/book", actPath+"/..", 1)
+			}),
+		),
+	)
+
+	chapters, err := getChapters(&cv, mdPath)
+	if err != nil {
+		panic(err)
+	}
+
+	err = createChapters(book, chapters, cssPath, "")
+	if err != nil {
+		panic(err)
+	}
+
+	// Save the EPUB book
+	err = book.Write("./il-manuale-del-buon-dev.epub")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createChapters(book *epub.Epub, chapters []Chapter, cssPath string, parent string) error {
+	for _, chapter := range chapters {
+		// Add a chapter
+		baseName := ""
+		if parent != "" {
+			// Add a sub-chapter
+			baseName = parent + "__"
+		}
+		baseName += chapter.Filename
+
+		fileName := baseName + ".xhtml"
+
+		var err error
+		if parent != "" {
+			// Add a sub-chapter
+			_, err = book.AddSubSection(parent, chapter.Html, chapter.Meta.Title, fileName, cssPath)
+		} else {
+			_, err = book.AddSection(chapter.Html, chapter.Meta.Title, fileName, cssPath)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		// Recursively add child chapters
+		if len(chapter.Children) > 0 {
+			err := createChapters(book, chapter.Children, cssPath, fileName)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+type Chapter struct {
+	Filename string
+	Meta     ChapterMeta
+	Content  string
+	Html     string
+	Children []Chapter
+}
+
+type ChapterMeta struct {
+	Title string `yaml:"title"`
+	Order int    `yaml:"nav_order"`
+}
+
+func getChapters(cv *goldmark.Markdown, mdPath string) (list []Chapter, err error) {
+	// Read markdown files from the directory
+	files, err := fs.ReadDir(os.DirFS(mdPath), ".")
+	if err != nil {
+		return
+	}
+
+	for _, file := range files {
+		if !file.IsDir() && filepath.Ext(file.Name()) == ".md" {
+			content, err := os.ReadFile(filepath.Join(mdPath, file.Name()))
+			if err != nil {
+				panic(err)
+			}
+
+			// Remove Jekyll-specific codes
+			tempContent := string(content)
+			tempContent = regexp.MustCompile(`\{:[^}]*\}`).ReplaceAllString(tempContent, "")
+			tempContent = strings.ReplaceAll(tempContent, "- TOC", "")
+			content = []byte(tempContent)
+
+			ctx := parser.NewContext()
+			var buf bytes.Buffer
+			if err := (*cv).Convert(content, &buf, parser.WithContext(ctx)); err != nil {
+				panic(err)
+			}
+
+			d := frontmatter.Get(ctx)
+
+			meta := ChapterMeta{}
+			err = d.Decode(&meta)
+			if err != nil {
+				return nil, err
+			}
+
+			html := buf.String()
+			html = strings.ReplaceAll(html, "<br>", "<br/>")
+
+			fileName := strings.TrimSuffix(file.Name(), ".md")
+
+			ch := Chapter{
+				Filename: fileName,
+				Meta:     meta,
+				//Content:  string(content),
+				Html: html,
+			}
+
+			// Add children chapters if any
+			dirPath := filepath.Join(mdPath, file.Name()[:len(file.Name())-3])
+			if info, err := os.Stat(dirPath); err == nil && info.IsDir() {
+				children, err := getChapters(cv, dirPath)
+				if err != nil {
+					return nil, err
+				}
+				ch.Children = children
+			}
+
+			list = append(list, ch)
+		}
+	}
+
+	slices.SortFunc(list, func(a, b Chapter) int {
+		return a.Meta.Order - b.Meta.Order
+	})
+
+	return list, nil
+}
